c9213a24303b1408ac643bb6bed475e87915c765
Helder Norte <hnorte@solocal.com>
2024-02-26 22:46:22 +0100

    feat: add basic quicklink-details page and routing

--- src/app/home/ui/checklist-list.component.ts
+++ src/app/home/ui/checklist-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { Checklist } from '../../shared/interfaces/checklist';
 
 @Component({
@@ -8,13 +9,16 @@ import { Checklist } from '../../shared/interfaces/checklist';
     <ul>
       @for (checklist of checklists; track checklist.id) {
       <li>
-        {{ checklist.title }}
+        <a routerLink="/checklist/{{ checklist.id }}">
+          {{ checklist.title }}
+        </a>
       </li>
       } @empty {
       <p>Click the add button to create your first checklist!</p>
       }
     </ul>
   `,
+  imports: [RouterLink],
 })
 export class ChecklistListComponent {
   @Input({ required: true }) checklists!: Checklist[];
